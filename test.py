65c07cda1a2d71dc6bdd6d181133cbfcd610bb2a
AlessandraColli <36989267+AlessandraColli@users.noreply.github.com>
2023-04-28 14:04:15 +0200

    update test

--- test.py
+++ test.py
@@ -36,7 +36,7 @@ def make_trend_signal(signal, length=252):
 
 
 if __name__ == '__main__':
-    
+
     today = datetime.datetime.today()
     today_ts = int(time.mktime(today.timetuple()))
 
@@ -51,13 +51,28 @@ if __name__ == '__main__':
 
     danger_spy_vol_thrs = 0.03
 
-    spy_price = spy['Adj Close'].iloc[-12:]
+    spy_price = spy['Adj Close']
     spy_rets = np.log(spy_price) - np.log(spy_price.shift(1))
+
+    spy_vol_3m = spy_rets.rolling(63).std(ddof=0)
+    vol_signal_126 = make_trend_signal(spy_vol_3m, length=126)
+    vol_signal_126 = vol_signal_126.fillna(method='ffill')
+
+    risk_signal = (vol_signal_126 == -1).astype(int)
+    if risk_signal.iloc[-1] == 0:
+        print("Risk off is active")
+
     spy_vol = spy_rets.iloc[-10:].std(ddof=0)
     if spy_vol > danger_spy_vol_thrs:
-        print("SPY rolling 10d volatility is above threshold.")
+        print(f"SPY rolling 10d volatility is above {danger_spy_vol_thrs}")
 
     ief_signal_252 = make_trend_signal(ief['Adj Close'])
 
-    print("IEF 252d trend signal is", ief_signal_252.iloc[-1])
- 
+    if ief_signal_252.iloc[-1] == 1:
+        print("Low yields regime")
+    elif ief_signal_252.iloc[-1] == -1:
+        print("High yields regime")
+    else:
+        print("Neutral regime")
+
+    print("End of process.")
