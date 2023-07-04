6ee643628bf3b7814196ea398486caa20af1cd39
AlessandraColli <36989267+AlessandraColli@users.noreply.github.com>
2023-07-04 10:32:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def make_trend_signal(signal, length=252):
     criterion = pd.Series(index=signal.index)
 
     for i, d in enumerate(ief.index):
-        if i <= 252:
+        if i <= length:
             continue
         mads = mad(signal.iloc[i - (length + 1):i])
         mean = signal.iloc[i - (length + 1):i].sum(skipna=True) / length
@@ -46,12 +46,19 @@ if __name__ == '__main__':
     url_ief = f'https://query1.finance.yahoo.com/v7/finance/download/IEF?period1=1650428990&period2={today_ts}&interval=1d&events=history&includeAdjustedClose=true'
     df_ief = pd.read_csv(url_ief)
 
+    url_shy = f'https://query1.finance.yahoo.com/v7/finance/download/SHY?period1=1650428990&period2={today_ts}&interval=1d&events=history&includeAdjustedClose=true'
+    df_shy = pd.read_csv(url_ief)
+
     spy = df_spy.set_index('Date')
     ief = df_ief.set_index('Date')
+    shy = df_shy.set_index('Date')
+
+    spy_price = spy['Adj Close'].fillna(method='ffill')
+    ief_price = ief['Adj Close'].fillna(method='ffill')
+    shy_price = shy['Adj Close'].fillna(method='ffill')
 
     danger_spy_vol_thrs = 0.03
 
-    spy_price = spy['Adj Close']
     spy_rets = np.log(spy_price) - np.log(spy_price.shift(1))
 
     spy_vol_3m = spy_rets.rolling(63).std(ddof=0)
@@ -60,13 +67,15 @@ if __name__ == '__main__':
 
     risk_signal = (vol_signal_126 == -1).astype(int)
     if risk_signal.iloc[-1] == 0:
-        print("Risk off is active")
+        print("Volatility risk off is active")
 
     spy_vol = spy_rets.iloc[-10:].std(ddof=0)
     if spy_vol > danger_spy_vol_thrs:
         print(f"SPY rolling 10d volatility is above {danger_spy_vol_thrs}")
 
-    ief_signal_126 = make_trend_signal(ief['Adj Close'], length=126)
+    # ief_signal_252 = make_trend_signal(ief['Adj Close'])
+    ief_signal_126 = make_trend_signal(ief_price, length=126)
+    shy_signal_126 = make_trend_signal(shy_price, length=126)
 
     if ief_signal_126.iloc[-1] == 1:
         print("Low yields regime")
@@ -74,5 +83,15 @@ if __name__ == '__main__':
         print("High yields regime")
     else:
         print("Neutral regime")
+
+    if shy_signal_126.iloc[-1] == 1:
+        print("Short term bonds up")
+
+    bonds_regime_signal = pd.Series(0.0, index=ief_signal_126.index)
+    bonds_regime_signal[(ief_signal_126 != -1) & (shy_signal_126 == -1)] = 1
+    combined_risk_signal = 1 - ((bonds_regime_signal == 0) & (risk_signal == 0)).astype(int)
+
+    if combined_risk_signal.iloc[-1] == 0:
+        print("Combined risk off is active")
 
     print("End of process.")
